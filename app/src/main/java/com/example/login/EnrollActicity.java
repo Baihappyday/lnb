69c76ef47792725e48870e45c4f3f02f4cd2d6e6
Baihappyday <2098704205@qq.com>
2022-03-11 17:14:35 +0800

    Creating

---
+++ app/src/main/java/com/example/login/EnrollActicity.java
@@ -0,0 +1,29 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+
+public class EnrollActicity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eoroll);
+        Button return_login = (Button)findViewById(R.id.return_login);
+        return_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Intent是一种运行时绑定（run-time binding）机制，它能在程序运行过程中连接两个不同的组件。
+                //在存放资源代码的文件夹下下，
+                Intent i = new Intent(EnrollActicity.this, MainActivity.class);
+                //启动
+                startActivity(i);
+            }
+        });
+    }
+}
