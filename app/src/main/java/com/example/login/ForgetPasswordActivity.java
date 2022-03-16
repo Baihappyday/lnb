3f1fb0e2f251f703e423ed781926b2c8382822df
MeLawSS <1210627584@qq.com>
2022-03-16 21:41:45 +0800

    change

--- app/src/main/java/com/example/login/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/login/ForgetPasswordActivity.java
@@ -15,15 +15,5 @@ public class ForgetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-        TextView textView = (TextView)findViewById(R.id.textView);
-        ll = (LinearLayout)findViewById(R.id.ll) ;
-        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams( LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
-        layoutParam.setMargins(0, height/4, 0, 0);
-        ll.setLayoutParams(layoutParam);
     }
 }
