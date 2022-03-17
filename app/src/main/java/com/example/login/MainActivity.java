52db05d4ec2a9247e9e019361277173818ba522e
MeLawSS <1210627584@qq.com>
2022-03-17 21:23:47 +0800

    Merge branch 'main' of https://github.com/Baihappyday/lnb
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button skip;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {//app的logo或者推广广告展示界面
@@ -17,7 +18,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.main_layout);
         skip = (Button)findViewById(R.id.skip);
         skip.setOnClickListener(this);
-        new Handler().postDelayed(new Runnable() {//5秒后自动跳转
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {//5秒后自动跳转
             @Override
             public void run() {
                 Intent intent = new Intent();
@@ -32,9 +34,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (findViewById(R.id.skip) == v){
             Intent intent = new Intent();
+            handler.removeCallbacksAndMessages(null);//清除handler中的延时操作
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setClass(MainActivity.this,IdentiChooseActivity.class);
             startActivity(intent);
+
         }
 
     }
