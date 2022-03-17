9ad0c6fb01c18db94f6346fe8c4157ffba46fd27
MeLawSS <1210627584@qq.com>
2022-03-17 21:33:10 +0800

    Merge branch 'main' of https://github.com/Baihappyday/lnb
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (findViewById(R.id.skip) == v){
             Intent intent = new Intent();
             handler.removeCallbacksAndMessages(null);//清除handler中的延时操作
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);//在跳转到目标activity前销毁所以已经存在的activity（实现不能返回的功能）
             intent.setClass(MainActivity.this,IdentiChooseActivity.class);
             startActivity(intent);
 
