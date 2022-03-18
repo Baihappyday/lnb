22e9554bc299218cd32db651e015b0f1486cff18
Baihappyday <101316028+Baihappyday@users.noreply.github.com>
2022-03-18 18:54:55 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/login/worker/workwalletActivity.java
@@ -0,0 +1,16 @@
+package com.example.login.worker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login.R;
+
+public class workwalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inwallet);
+    }
+}
