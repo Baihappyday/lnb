48a56bf4abc52ec384fcfdb129249c4dc00d8d38
MeLawSS <1210627584@qq.com>
2022-03-18 14:55:38 +0800

    Merge branch 'main' of https://github.com/Baihappyday/lnb
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/login/user/ModifyUserlInfo.java
+++ app/src/main/java/com/example/login/user/ModifyUserlInfo.java
@@ -2,25 +2,32 @@ package com.example.login.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.login.R;
 
+import java.util.Calendar;
 import java.util.List;
 
 
-public class ModifyUserlInfo extends AppCompatActivity {
+public class ModifyUserlInfo extends AppCompatActivity implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+    private TextView tv_birthdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_userl_info);
 
+        tv_birthdate = (TextView)findViewById(R.id.birthdate);
+        tv_birthdate.setOnClickListener(this);
         ArrayAdapter<String> provinceAdapter = new ArrayAdapter<String>(this,
                 R.layout.item_select, provinceArray);
         ArrayAdapter<String> cityAdapter = new ArrayAdapter<String>(this,
@@ -53,9 +60,6 @@ public class ModifyUserlInfo extends AppCompatActivity {
     private String[] provinceArray = {"北京市", "河北省", "安徽省", "河南省", "湖北省", "湖南省"};
     private String[] cityArray = {"北京市", "郑州市", "合肥市", "武汉市", "长沙市", "芜湖市"};
     private String[] countyArray = {"北京市", "郑州市", "合肥市", "武汉市", "长沙市", "芜湖市"};
-    private List<Integer> yearArray;
-    private List<Integer> monthArray;
-    private List<Integer> dayArray;
 
     class MySelectedListener implements AdapterView.OnItemSelectedListener {
         public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
@@ -64,5 +68,24 @@ public class ModifyUserlInfo extends AppCompatActivity {
 
         public void onNothingSelected(AdapterView<?> arg0) {
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.birthdate) {
+            Calendar calendar = Calendar.getInstance();
+            DatePickerDialog dialog = new DatePickerDialog(this, this,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH));
+            dialog.show();
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        String desc = String.format("您选择的日期是%d年%d月%d日",
+                year, monthOfYear+1, dayOfMonth);
+        tv_birthdate.setText(desc);
     }
 }
