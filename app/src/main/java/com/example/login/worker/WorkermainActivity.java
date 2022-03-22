11dcdeac20a96fe730da603edc9223527634995e
Baihappyday <2098704205@qq.com>
2022-03-22 14:03:17 +0800

    update

---
+++ app/src/main/java/com/example/login/worker/WorkermainActivity.java
@@ -0,0 +1,21 @@
+package com.example.login.worker;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.login.R;
+
+public class WorkermainActivity extends Fragment{
+    protected View v;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_worker_main, container, false);
+        return v;
+    }
+}
